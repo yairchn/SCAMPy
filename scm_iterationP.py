58d73835b31fb4fffc89b7115626bbc1d5ce8cd7
Yair Cohen <yairchn@caltech.edu>
2017-11-11 13:49:18 -0800

    testing subprocess text

--- scm_iterationP.py
+++ scm_iterationP.py
@@ -29,7 +29,7 @@ def scm_iterP(ncore, true_data, theta,  case_name, geom_opt=0):
     print('dst',dst)
     file_namelist = open(dst,'r')
     print('type(file_namelist) in scm_iter',type(file_namelist))
-    namelist = json.loads(file_namelist)
+    namelist = json.loads(dst)
     print('json load works', dst)
     # receive parameter value and generate paramlist file for new data
     paramlist = MCMC_paramlist(theta, case_name+ncore)
