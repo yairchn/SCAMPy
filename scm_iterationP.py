9dfcfb4e8206952ad51408458450b27948b0552d
Yair Cohen <yairchn@caltech.edu>
2017-11-15 11:47:04 -0800

    writing netcdf output

--- scm_iterationP.py
+++ scm_iterationP.py
@@ -189,10 +189,9 @@ def create_record(theta_, costFun_, new_data, new_dir):
         _cloud_cover[0:dim,:] = cloud_cover1_
         _cloud_top[0:dim,:] = cloud_top1_
         _cloud_base[0:dim,:] = cloud_base1_
+        print(np.shape(thetal1_))
+        print(np.shape(_thetal))
         _thetal[0:dim,:,:] = thetal1_
-        print(np.shape(theta1_))
-        print(np.shape(_theta))
-
         _theta[0:dim,:] = theta1_
         _costFun[0:dim,:] = costFun1_
 
