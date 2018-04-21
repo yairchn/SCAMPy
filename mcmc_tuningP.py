d5c60c9d80e1f7f0d5ce7feb438f7592ff34d7a3
Yair Cohen <yairchn@caltech.edu>
2018-04-21 14:08:19 -0700

    ..

--- mcmc_tuningP.py
+++ mcmc_tuningP.py
@@ -50,7 +50,7 @@ def main():
     costFun = lambda theta, geom_opt: scm_iterationP.scm_iterP(ncore,true_data, theta, case_name, fname , model_type , txt, geom_opt)
     #tuning_log.write("define Lambda as scm_iter")
     # set boudaries for the mcmc
-    #uppbd = 2.0 * np.ones(args.D)
+    uppbd = 3.0 * np.ones(args.D)
     lowbd = 0.0 * np.ones(args.D)
     #if lowbd>=uppbd:
     #    sys.exit('lowbd must be smaller than uppbd')
@@ -60,7 +60,7 @@ def main():
 
     # call Parallel_mcmc.py
     mc_fun = geoMC.geoMC(theta0, costFun, args.algs[args.algNO],
-                         args.step_sizes[args.algNO], args.step_nums[args.algNO],lowbd, [],
+                         args.step_sizes[args.algNO], args.step_nums[args.algNO],lowbd, uppbd,
                          'reject').sample # try reject here rather than bounce
 
 
