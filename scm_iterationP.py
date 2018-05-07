4085f77adefff6ddf4da21c1fdab5c4e958b738a
Yair Cohen <yairchn@caltech.edu>
2018-05-07 15:45:49 -0700

    revised costFun with CT_temp

--- scm_iterationP.py
+++ scm_iterationP.py
@@ -199,8 +199,8 @@ def generate_costFun(theta, true_data,new_data, fname, model_type):
     dlwp = np.mean(s_lwp[ts1:], 0) - np.mean(p_lwp[ts1:], 0)
 
     rnoise = 1.0
-    f = np.diag([dlwp, dCF, dCT_temp])
-    sigma = np.multiply(rnoise, np.diag([1 / var_lwp, 1 / var_CF, 1 / var_CT_temp]))
+    f = np.diag([dlwp, dCF, dCT])
+    sigma = np.multiply(rnoise, np.diag([1 / var_lwp, 1 / var_CF, 1 / var_CT]))
     J0 = np.divide(np.linalg.norm(np.dot(sigma, f), ord=None), 2.0)  # ord=None for matrix gives the 2-norm
     m=0.2
     s = 0.5
